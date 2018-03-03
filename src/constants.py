a5a0819dfdad537f10775b0c59dc447c07683c4b
Lukas Manduch <manduch.lukas@gmail.com>
2018-03-03 22:43:57 +0100

    Add new constants for Add command

--- src/constants.py
+++ src/constants.py
@@ -6,8 +6,13 @@ PROMPT_SYMBOL = '> '
 # Main arguments
 COMMAND = 'command'
 
+# Search
 COMMAND_SEARCH = 'search'
 COMMAND_SEARCH_VALUE = 'term'
+# Add
+COMMAND_ADD = 'add'
+COMMAND_ADD_KEY = 'key'
+COMMAND_ADD_VALUE = 'value'
 
 
 
@@ -19,5 +24,9 @@ UNHANDLED_EXCEPTION = "Sorry, unhandled exception occured.\
 
 SEARCH_INTERACTION_PROMPT = "Search"
 SEARCH_INTERACTION_INFO = "Please enter search term"
+
+ADD_INTERACTION_KEY_MISSING = "Command - Add\
+\nPlease enter key which will represent this entry.\
+\nThen enter value. Value must end with two empty lines"
 
 HELP_AMBIGUOUS = "Sorry command is ambiguous:\n"
