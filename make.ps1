1364f3bcc82b0796eda101a157d125e57134eae5
Lukas Manduch <manduch.lukas@gmail.com>
2018-04-14 18:21:36 +0200

    Create powershell make command for Windows

---
+++ make.ps1
@@ -0,0 +1,24 @@
+﻿param($lint = $false)
+
+$Script:ErrorActionPreference = "Continue"
+
+$interpreter = get-command python | Select-Object -ExpandProperty Source  
+#Write-Host "Using interpreter $interpreter"
+$args = @('-m', 'unittest', 'discover', '-s', 'src', '-p', '*_test.py')
+
+& $interpreter $args 2>&1 | % { "$_" }
+
+if ($lint -eq $false -or $LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Get-ChildItem "src" -File -Filter "*.py" |
+ForEach-Object {
+    &"python" "-m", "pylint", $_.FullName 2>&1 | % { "$_" }
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+}
+
+exit $LASTEXITCODE
